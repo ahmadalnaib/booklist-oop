88ce95e5e18eec8d97b68aab54823a20840f378b
ahmedhabeeb73 <az73system@gmail.com>
2019-12-24 18:31:12 +0100

    validate

--- app.js
+++ app.js
@@ -26,11 +26,22 @@ UI.prototype.addBookToList=function(book){
   list.appendChild(row);
 }
 
+UI.prototype.showAlert=function(msg,className){
+  const div=document.createElement("div");
+  div.className=`alert ${className}`;
+  div.appendChild(document.createTextNode(msg));
+  
+  const container=document.querySelector(".container");
+  const form=document.querySelector("#book-form ");
+  container.insertBefore(div,form);
+  }
+
 UI.prototype.clearFields=function(){
   document.querySelector("#title").value='';
   document.querySelector("#author").value='';
   document.querySelector("#isbn").value='';
 }
+
 
 
 // EVENT LISTENERS
@@ -46,10 +57,19 @@ document.querySelector("#book-form").addEventListener("submit",function(e){
 //  instantiate UI
 const ui= new UI();
 
+
+// validate
+if(title==="" || author==="" || isbn===""){
+
+  ui.showAlert('pleass fill all fields',"error");
+
+}else{
+
 // add book to ui
 ui.addBookToList(book);
 
 ui.clearFields();
+}
 
 
   e.preventDefault();
